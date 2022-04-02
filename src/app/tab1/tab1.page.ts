9fe7234c6228572583838664fc7d3b5cfa8dd4e1
Diego137-ux <62929033+Diego137-ux@users.noreply.github.com>
2022-04-01 19:34:51 -0600

    5 marzo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnChanges {
 
   constructor() {}
-
+  ngOnChanges(): void {
+  }
   alumnos = [
     {
       "nombre": "Abraham",
@@ -65,11 +66,40 @@ export class Tab1Page {
       "apellido": "Castillo",
       "matricula": "ABC123"
     },
-    {
-      "nombre": "Ricardo",
-      "apellido": "Rocha",
-      "matricula": "ABC123"
-    },
+    
   
   ];
-}
+
+  @Input() nombre: string = "";
+  @Input() apellido: string = "";
+  @Input() matricula: string = "";
+
+
+  agregarAlumno(): void {
+    var nuevoAlumno: any = {
+      "nombre": this.nombre,
+      "apellido": this.apellido,
+      "matricula": this.matricula
+    }
+
+    this.alumnos.push(nuevoAlumno);
+
+    this.alumnos.sort(function(a, b) {
+      var nameA = a.nombre.toUpperCase(); 
+      var nameB = b.nombre.toUpperCase(); 
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+  
+      return 0;
+    });
+  
+      console.log(this.alumnos); 
+    }
+
+   
+  }
+
